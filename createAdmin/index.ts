7f52a8febcf5aabbe92cb25ad52ea1fe1afe6cfa
RaghhavDTurki <raghhavdturki@gmail.com>
2022-05-23 19:33:20 +0530

    Add Create Admin Feature

---
+++ createAdmin/index.ts
@@ -0,0 +1,48 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import { connect } from "../src/config/db.config";
+import { sentryInit } from "../src/config/sentry.config";
+import * as Sentry from "@sentry/node";
+import { AdminSignupInput } from "../src/types/ValidationInput";
+import { adminSignup } from "../src/controller/Admin/adminSignup";
+
+const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+    const HEADERS = {'Content-Type': 'application/json'};
+    connect();
+    sentryInit();
+    try{    
+        const body: AdminSignupInput = req.body;
+        const result = await adminSignup(body);
+        if(result){
+            context.res = {
+                status: 400,
+                body: {
+                    message: result.message
+                },
+                headers: HEADERS
+            };
+        }
+        else{
+            context.res = {
+                status: 200,
+                body: {
+                    message: "Admin created successfully!",
+                },
+                headers: HEADERS
+            };
+        }
+    }
+    catch(err){
+        Sentry.captureException(err);
+        await Sentry.flush(2000);
+        context.res = {
+            status: 500,
+            body: {
+                message: err.message
+            },
+            headers: HEADERS
+        };
+    }
+
+};
+
+export default httpTrigger;
---
+++ src/controller/Admin/adminSignup.ts
@@ -0,0 +1,29 @@
+import { AdminSignupInput } from "../../types/ValidationInput";
+import * as Sentry from "@sentry/node";
+import { Admin } from "../../models/Admin.model";
+export const adminSignup = async (body:AdminSignupInput) => {
+    try{
+        if(await Admin.findOne({ email: body.email })){
+            return {
+                error: true,
+                message: "Admin already exists!"
+            }
+        }
+        const newAdmin = new Admin();
+        newAdmin.email = body.email;
+        newAdmin.password = body.password;
+        newAdmin.profile.name = body.name;
+        newAdmin.profile.email = body.email;
+        newAdmin.profile.contactno = body.contactno;
+        newAdmin.adminid = body.adminid;
+        await newAdmin.save();
+    }
+    catch(err){
+        Sentry.captureException(err);
+        await Sentry.flush(2000);
+        return {
+            error: true,
+            message: err.message
+        }
+    }
+}
