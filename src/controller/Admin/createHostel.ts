cf56d9fe53a6ac575bba1dd6104dcb3ea72abe40
RaghhavDTurki <raghhavdturki@gmail.com>
2022-05-23 19:00:51 +0530

    Fix Path for create hostel

---
+++ src/controller/Admin/createHostel.ts
@@ -0,0 +1,38 @@
+import { HostelDocument, HostelModel } from "../../models/Hostel.model";
+import * as Sentry from "@sentry/node";
+import { CreateHostelInput } from "../../types/ValidationInput";
+
+export async function createHostel(body: CreateHostelInput){
+    let hostel: HostelDocument;
+    try{
+        hostel = new HostelModel();
+        hostel.hostelid = body.hostelid;
+        hostel.name = body.name;
+        hostel.location = body.location;
+        hostel.description = body.description;
+        hostel.totalCapacity = +body.totalCapacity;
+        if(body.singleRooms){
+            hostel.singleRooms = +body.singleRooms;
+            hostel.singleRoomsLeft = +body.singleRooms;
+        }
+        if(body.doubleRooms){
+            hostel.doubleRooms = +body.doubleRooms;
+            hostel.doubleRoomsLeft = +body.doubleRooms;
+        }
+        if(body.tripleRooms){
+            hostel.tripleRooms = +body.tripleRooms;
+            hostel.tripleRoomsLeft = +body.tripleRooms;
+        }
+        hostel.fees = +body.fees;
+        
+        if(body.wardenid){
+            hostel.wardenid = body.wardenid;
+        }
+        await hostel.save()
+    }
+    catch(err){
+        Sentry.captureException(err);
+        await Sentry.flush(2000);
+        return true;
+    }
+}
