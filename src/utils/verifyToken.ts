e389a3aa97773fa88a516ea5270ddaf88a35cefa
RaghhavDTurki <raghhavdturki@gmail.com>
2022-05-24 10:41:18 +0530

    Add Role Auth in token verification

--- src/utils/verifyToken.ts
+++ src/utils/verifyToken.ts
@@ -3,7 +3,7 @@ import * as Sentry from '@sentry/node';
 
 const secretToken = process.env.SECRET_TOKEN;
 
-export const verifyToken = async (token: string) => {
+export const verifyToken = async (token: string, role?: string) => {
     try{
         const bearer = token.split(" ");
         if(bearer.length !== 2){
@@ -14,6 +14,14 @@ export const verifyToken = async (token: string) => {
         }
 
         const unsealed = await iron.unseal(bearer[1], secretToken, iron.defaults);
+        if(role){
+            if(unsealed.role !== role){
+                return {
+                    error: true,
+                    message: "Unauthorised Access!"
+                }
+            }
+        }
         return {
             error: false,
             message: unsealed
