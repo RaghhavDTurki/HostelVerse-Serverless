8a0b9328b241dbb86c6ecee6d3c0067b04df78e7
RaghhavDTurki <raghhavdturki@gmail.com>
2022-05-27 16:48:02 +0530

    Add Feature View Announcement

---
+++ getAnnouncements/index.ts
@@ -0,0 +1,79 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import * as Sentry from "@sentry/node";
+import { connect } from "../src/config/db.config";
+import { sentryInit } from "../src/config/sentry.config";
+import { viewAnnouncement } from "../src/controller/Student/viewAnnouncement";
+import { verifyToken } from "../src/utils/verifyToken";
+
+const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+    const HEADERS = { "Content-Type": "application/json" };
+    connect();
+    sentryInit();
+    try{
+        // Check for Token in Headers
+        const authToken = req.headers.authorization;
+        if(!authToken){
+            context.res = {
+                status: 401,
+                body: {
+                    message: "No authorization token provided!"
+                },
+                headers: HEADERS
+            };
+            return;
+        }
+        const unsealedToken = await verifyToken(authToken, "student");
+        if(unsealedToken.error){
+            context.res = {
+                status: 401,
+                body: {
+                    message: "Unauthorized!"
+                },
+                headers: HEADERS
+            };
+            return;
+        }
+        const studentid = req.query.studentid;
+        if(!studentid){
+            context.res = {
+                status: 400,
+                body: {
+                    error: true,
+                    message: "Student id is required!"
+                }
+            };
+            return;
+        }
+        const announcement = await viewAnnouncement(studentid);
+        if(announcement.error){
+            context.res = {
+                status: 400,
+                body: {
+                    message: announcement.message
+                },
+                headers: HEADERS
+            }
+            return;
+        }
+        context.res = {
+            status: 200,
+            body: {
+                data: announcement.data
+            },
+            headers: HEADERS
+        };
+    }
+    catch(err){
+        Sentry.captureException(err);
+        await Sentry.flush(2000);
+        context.res = {
+            status: 500,
+            body: {
+                message: err
+            },  
+            headers: HEADERS
+        };
+    }
+};
+
+export default httpTrigger;
---
+++ src/controller/Student/viewAnnouncement.ts
@@ -0,0 +1,44 @@
+import * as Sentry from '@sentry/node';
+import { Student } from '../../models/Student.model';
+import { Announcement } from '../../models/Announcement.model';
+
+export const viewAnnouncement = async (studentid: string) => {
+    try{
+        if(!studentid){
+            return {
+                error: true,
+                message: "Student id is required!"
+            }
+        }
+        const student = await Student.findOne({
+            studentId: studentid
+        }).select("-_id -__v").lean();
+        if(!student){
+            return {
+                error: true,
+                message: "Student not found!"
+            }
+        }
+        const announcement = await Announcement.findOne({
+            hostelId: student.hostelid
+        }).select("-_id -__v").lean();
+        if(!announcement){
+            return {
+                error: true,
+                message: "No announcements found!"
+            }
+        }
+        return {
+            error: false,
+            data: announcement
+        };
+    }
+    catch(err){
+        Sentry.captureException(err);
+        await Sentry.flush(2000);
+        return {
+            error: true,
+            message: err
+        };                  
+    }
+}
