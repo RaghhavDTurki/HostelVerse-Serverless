fe368aa3de74b7d6b2d2a2db0ac6eb6da359bf14
RaghhavDTurki <raghhavdturki@gmail.com>
2022-05-27 16:49:48 +0530

    Fix Student Signup

--- src/controller/Student/StudentSignup.ts
+++ src/controller/Student/StudentSignup.ts
@@ -1,22 +1,10 @@
-import axios from "axios";
 import { Attendence } from "../../models/Attendence.model";
 import { Student } from "../../models/Student.model";
 import { CreateStudentInput } from "../../types/ValidationInput";
 import { createOTP } from "../../utils/createOTP";
 import { sendOTPEmail } from "../../utils/mailer";
 import * as Sentry from '@sentry/node';
-
-const MAP_QUEST_KEY = process.env.MAP_QUEST_KEY
-const college_location = "26.9124, 75.7873";
-async function getDistance(location: string): Promise<number> {
-    // const query_url = `http://www.mapquestapi.com/directions/v2/route?key=${MAP_QUEST_KEY}&unit=k&from=${location}&to=${college_location}`;
-    const query_url = `http://www.mapquestapi.com/directions/v2/route?key=${MAP_QUEST_KEY}&from=${location}&to=${college_location}`;
-    // console.log(query_url);
-    const distance = (await axios.get(query_url)).data;
-    // console.log(distance);
-    const answer = distance.route.distance;
-    return answer;
-}
+import { getDistance } from "../../utils/getDistance";
 
 export const signupStudent = async (body: CreateStudentInput) => {
     try{
@@ -29,27 +17,17 @@ export const signupStudent = async (body: CreateStudentInput) => {
             };
         }
     
+        
         const OTP = createOTP();
         const expiry = Date.now() + 60 * 1000 * 15;  //Set expiry 15 mins ahead from now
-        const sendOTP = await sendOTPEmail(body.email, OTP);
-        if(sendOTP.error){
-            return { 
-                error: true, 
-                message: "Couldn't send verification email."
-            };
-        }
-    
-        const studentEntry = new Attendence({
-            studentid: body.studentid
-        });
-        await studentEntry.save();
-    
+        
         const distance = await getDistance(body.location);
-    
+        
         const student = new Student();
         student.email = body.email;
         student.password = body.password;
         student.studentid = body.studentid;
+        student.batch = body.studentid.substring(0, 4);
         student.profile.name = body.name;
         student.profile.gender = body.gender;
         student.profile.email = body.email;
@@ -59,6 +37,19 @@ export const signupStudent = async (body: CreateStudentInput) => {
         student.emailTokenExpires = new Date(expiry);
         student.distance = distance;
         student.save();
+
+        const studentEntry = new Attendence({
+            studentid: body.studentid
+        });
+        await studentEntry.save();
+        
+        const sendOTP = await sendOTPEmail(body.email, OTP);
+        if(sendOTP.error){
+            return { 
+                error: true, 
+                message: "Couldn't send verification email."
+            };
+        }
     }
     catch(err){
         Sentry.captureException(err);
