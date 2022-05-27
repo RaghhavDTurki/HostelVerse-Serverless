71c41032b8c5f75c02a9e898abedb13c0eadb20f
RaghhavDTurki <raghhavdturki@gmail.com>
2022-05-27 16:42:18 +0530

    Add Create Announcement

---
+++ src/controller/Warden/Announcements.ts
@@ -0,0 +1,31 @@
+import * as Sentry from "@sentry/node";
+import { Warden } from "../../models/Warden.model";
+import { Announcement } from "../../models/Announcement.model";
+import { CreateAnnouncementInput } from "../../types/ValidationInput";
+
+export const createAnnouncement = async (body: CreateAnnouncementInput) => {
+    try {
+        const wardenHostel = await Warden.findOne({ wardenid: body.wardenid }).select("hostelid").lean();
+        if (!wardenHostel) {
+            return {
+                error: true,
+                message: "Warden not found!"
+            }
+        }
+        await Announcement.create({
+            ...body,
+            hostelid: wardenHostel.hostelid
+        });
+        return {
+            error: false,
+            message: "Announcement created successfully!"
+        };
+    } catch (err) {
+        Sentry.captureException(err);
+        await Sentry.flush(2000);
+        return {
+            error: true,
+            message: err
+        };
+    }
+}
