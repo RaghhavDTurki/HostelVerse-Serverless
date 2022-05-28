0a1b444f3d58cc35e27f63b56b5b8315cd0ce1f9
RaghhavDTurki <raghhavdturki@gmail.com>
2022-05-28 15:14:39 +0530

    Add Student Checkin Checkout feature

---
+++ checkin/index.ts
@@ -0,0 +1,69 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import * as Sentry from "@sentry/node";
+import { connect } from "../src/config/db.config";
+import { sentryInit } from "../src/config/sentry.config";
+import { checkIn } from "../src/controller/Student/CheckIn_CheckOut";
+import { CheckInInput } from "../src/types/ValidationInput";
+import { verifyToken } from "../src/utils/verifyToken";
+
+const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+    const HEADERS = { "Content-Type": "application/json" };
+    connect();
+    sentryInit();
+    try{
+        // Check for token
+        const authToken = req.headers.authorization;
+        if(!authToken){
+            context.res = {
+                status: 401,
+                body: {
+                    message: "No authorization token provided!"
+                },
+                headers: HEADERS
+            };
+            return;
+        }
+        const unsealedToken = await verifyToken(authToken, "student");
+        if(unsealedToken.error){
+            context.res = {
+                status: 401,
+                body: {
+                    message: "Unauthorized!"
+                },
+                headers: HEADERS
+            };
+            return;
+        }
+        const body: CheckInInput = req.body;
+        const result = await checkIn(body);
+        if(result.error){
+            context.res = {
+                status: 400,
+                body: result
+            }
+            return;
+        }
+        else{
+            context.res = {
+                status: 200,
+                body: {
+                    message: "Checked in successfully!"
+                }
+            };
+        }
+    }
+    catch(err){
+        Sentry.captureException(err);
+        await Sentry.flush(2000);
+        context.res = {
+            status: 500,
+            body: {
+                error: true,
+                message: "Something went wrong!"
+            }
+        };
+    }
+
+};
+
+export default httpTrigger;
---
+++ checkout/index.ts
@@ -0,0 +1,69 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import { connect } from "../src/config/db.config";
+import { sentryInit } from "../src/config/sentry.config";
+import { checkOut } from "../src/controller/Student/CheckIn_CheckOut";
+import { CheckOutInput } from "../src/types/ValidationInput";
+import { verifyToken } from "../src/utils/verifyToken";
+import * as Sentry from "@sentry/node";
+
+const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+    const HEADERS = { "Content-Type": "application/json" };
+    connect();
+    sentryInit();
+    try{
+        // Check for token
+        const authToken = req.headers.authorization;
+        if(!authToken){
+            context.res = {
+                status: 401,
+                body: {
+                    message: "No authorization token provided!"
+                },
+                headers: HEADERS
+            };
+            return;
+        }
+        const unsealedToken = await verifyToken(authToken, "student");
+        if(unsealedToken.error){
+            context.res = {
+                status: 401,
+                body: {
+                    message: "Unauthorized!"
+                },
+                headers: HEADERS
+            };
+            return;
+        }
+        const body: CheckOutInput = req.body;
+        const result = await checkOut(body);
+        if(result.error){
+            context.res = {
+                status: 400,
+                body: result
+            }
+            return;
+        }
+        else{
+            context.res = {
+                status: 200,
+                body: {
+                    message: "Checked out successfully!"
+                }
+            };
+        }
+    }
+    catch(err){
+        Sentry.captureException(err);
+        await Sentry.flush(2000);
+        context.res = {
+            status: 500,
+            body: {
+                error: true,
+                message: "Something went wrong!"
+            }
+        };
+    }
+
+};
+
+export default httpTrigger;
