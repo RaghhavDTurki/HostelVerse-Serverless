7faec8d58c0f1998edc14ca380b275e5943d33de
RaghhavDTurki <raghhavdturki@gmail.com>
2022-05-28 15:13:55 +0530

    Add Get Admin Profile feature

---
+++ getAdminProfile/index.ts
@@ -0,0 +1,86 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import * as Sentry from "@sentry/node";
+import { connect } from "../src/config/db.config";
+import { sentryInit } from "../src/config/sentry.config";
+import { verifyToken } from "../src/utils/verifyToken";
+import { getAdminProfile } from "../src/controller/Admin/adminProfile";
+
+const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+    const HEADERS = { "Content-Type": "application/json" };
+    connect();
+    sentryInit();
+    try{
+        // Check for Token in Headers
+        const authToken = req.headers.authorization;
+        if (!authToken) {
+            context.res = {
+                status: 401,
+                body: {
+                    message: "Unauthorised!"
+                },
+                headers: HEADERS
+            };
+            return;
+        }
+        const unsealedToken = await verifyToken(authToken, "admin");
+        if (unsealedToken.error) {
+            context.res = {
+                status: 401,
+                body: {
+                    message: "Unauthorized!"
+                },
+                headers: HEADERS
+            };
+            return;
+        }
+        const adminid = req.query.adminid;
+        if(adminid != unsealedToken.message.id)
+        {
+            context.res = {
+                status: 401,
+                body: {
+                    message: "Unauthorized!"
+                },
+                headers: HEADERS
+            };
+            return;
+        }
+        const result = await getAdminProfile(adminid);
+        if(result.error)
+        {
+            context.res = {
+                status: 400,
+                body: {
+                    message: result.message
+                },
+                headers: HEADERS
+            };
+            return;
+        }
+        else{
+            context.res = {
+                status: 200,
+                body: {
+                    error: false,
+                    data: result.data
+                },
+                headers: HEADERS
+            };
+            return;
+        }
+    }
+    catch(err){
+        Sentry.captureException(err);
+        await Sentry.flush(2000);
+        context.res = {
+            status: 500,
+            body: {
+                message: err
+            },
+            headers: HEADERS
+        };
+    }
+
+};
+
+export default httpTrigger;
---
+++ src/controller/Admin/adminProfile.ts
@@ -0,0 +1,36 @@
+import { Admin } from "../../models/Admin.model";
+import * as Sentry from "@sentry/node";
+
+export const getAdminProfile = async (adminid: string) => {
+    try{
+        if(!adminid)
+        {
+            return {
+                error: true,
+                message: "Admin id is required!"
+            }
+        }
+        const admin = await Admin.findOne({
+            adminid: adminid
+        }).select("-_id -__v").lean();
+        if(!admin)
+        {
+            return {
+                error: true,
+                message: "Admin not found!"
+            }
+        }
+        return {
+            error: false,
+            data: admin
+        };
+    }
+    catch(err){
+        Sentry.captureException(err);
+        await Sentry.flush(2000);
+        return {
+            error: true,
+            message: err
+        };
+    }
+}
