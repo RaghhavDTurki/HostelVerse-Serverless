9df5753ee235d2ded515b425838b3a178b4a7831
RaghhavDTurki <raghhavdturki@gmail.com>
2022-05-29 16:26:14 +0530

    Add attendence status in get student profile

--- src/controller/Student/studentProfile.ts
+++ src/controller/Student/studentProfile.ts
@@ -1,5 +1,45 @@
 import * as Sentry from "@sentry/node";
-import { Student } from "../../models/Student.model";
+import { Student, StudentDocument } from "../../models/Student.model";
+import { Attendence } from "../../models/Attendence.model";
+
+function isToday(date: Date) {
+    return date.getDate() === new Date().getDate() && date.getMonth() === new Date().getMonth() && date.getFullYear() === new Date().getFullYear();
+}
+async function getAttendenceStatus(Student: StudentDocument){
+    try{
+        const attendence = await Attendence.findOne({ studentid: Student.studentid }).select("-_id -__v").lean();
+        const lastCheckIn = attendence.last_checkin;
+        const lastCheckOut = attendence.last_checkout;
+        let studentLocation = "";
+        if(lastCheckIn == null || lastCheckOut == null){
+            studentLocation = "Not checked in";
+        }
+        if(!isToday(lastCheckIn || lastCheckOut)){
+            studentLocation = "Not in Hostel";
+        }
+        if(lastCheckOut > lastCheckIn || lastCheckOut == null){
+            studentLocation = "Not in Hostel";
+        }
+        else if(lastCheckIn.getHours() >= 21){
+            studentLocation = "Not in Hostel";
+        }
+        else{
+            studentLocation = "In Hostel";
+        }
+        return {
+            error: false,
+            data: studentLocation
+        }
+    }
+    catch(err){
+        Sentry.captureException(err);
+        await Sentry.flush(2000);
+        return {
+            error: true,
+            message: err
+        };
+    }       
+}
 
 export const getStudentProfile = async (studentid: string) => {
     try {
@@ -11,16 +51,20 @@ export const getStudentProfile = async (studentid: string) => {
         }
         const student = await Student.findOne({
             studentid: studentid
-        }).select("-_id -__v").lean();
+        }).select("-_id -__v");
         if(!student){
             return {
                 error: true,
                 message: "Student not found!"
             }
-        }
+        }   
+        const attendenceStatus = await getAttendenceStatus(student);
         return {
             error: false,
-            data: student
+            data: {
+                student,
+                attendenceStatus
+            }
         };
     } 
     catch (err) {
