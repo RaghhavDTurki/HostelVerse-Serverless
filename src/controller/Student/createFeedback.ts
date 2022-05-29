a7b5765c8aea2d9179af05fe4e6b4e4ace2e38a7
RaghhavDTurki <raghhavdturki@gmail.com>
2022-05-29 09:22:46 +0530

    Fix create feedback

--- src/controller/Student/createFeedback.ts
+++ src/controller/Student/createFeedback.ts
@@ -1,11 +1,26 @@
 import * as Sentry from "@sentry/node";
 import { Feedback } from "../../models/Feedback.model";
+import { Hostel } from "../../models/Hostel.model";
 import { CreateFeedbackInput } from "../../types/ValidationInput";
 
 export const createFeedback = async (body: CreateFeedbackInput) => {
     try {
+        const hostel = await Hostel.findOne({ hostelid: body.hostelid}).select("-__v");
+        if(!hostel){
+            return {
+                error: true,
+                message: "Hostel not found!"
+            }
+        }
         const feedback = new Feedback(body);
         await feedback.save();
+        const numReviews = hostel.numberOfReviews;
+        const overallRating = hostel.overallRating;
+        const newOverallRating = (overallRating * numReviews + body.rating) / (numReviews + 1);
+        const newNumReviews = numReviews + 1;
+        hostel.overallRating = newOverallRating;
+        hostel.numberOfReviews = newNumReviews;
+        await hostel.save();
     } 
     catch (err) {
         Sentry.captureException(err);
