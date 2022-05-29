fad1617ad8d76a61a6b28579d2abb6a0769ab385
RaghhavDTurki <raghhavdturki@gmail.com>
2022-05-29 09:42:59 +0530

    Fix delete warden

--- src/controller/Admin/deleteWarden.ts
+++ src/controller/Admin/deleteWarden.ts
@@ -1,4 +1,5 @@
 import * as Sentry from '@sentry/node';
+import { Hostel } from '../../models/Hostel.model';
 import { Warden } from '../../models/Warden.model';
 import { DeleteWardenInput } from '../../types/ValidationInput';
 
@@ -13,10 +14,21 @@ export const deleteWarden = async (body: DeleteWardenInput) => {
                 message: "Warden not found!"
             };
         }
-        return {
-            error: false,
-            message: "Warden deleted successfully!"
-        };
+        const hostel = await Hostel.findOne({ hostelId: warden.hostelid });
+        if(!hostel){
+            return {
+                error: false,
+                message: "Warden deleted successfully!"
+            };
+        }
+        else{
+            hostel.wardenid = null;
+            await hostel.save();
+            return {
+                error: false,
+                message: "Warden deleted successfully!"
+            };
+        }
     } 
     catch (err) {
         Sentry.captureException(err);
