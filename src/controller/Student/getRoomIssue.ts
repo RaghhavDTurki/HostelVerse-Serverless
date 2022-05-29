53957f7ed534c5a168b8b91f9d1d71a7608acd16
RaghhavDTurki <raghhavdturki@gmail.com>
2022-05-29 13:18:58 +0530

    Add Get Student Room Issue feature

---
+++ src/controller/Student/getRoomIssue.ts
@@ -0,0 +1,50 @@
+import * as Sentry from "@sentry/node"; 
+import { RoomIssue } from "../../models/RoomIssue.model";
+import { Student } from "../../models/Student.model";
+
+export const getStudentRoomIssue = async(studentid: string) => {
+    try{
+        if(!studentid){
+            return {
+                error: true,
+                message: "Student id is required!"
+            }
+        }
+        const student = await Student.findOne({
+            studentid: studentid
+        }).select("-_id -__v").lean();
+        if(!student){
+            return {
+                error: true,
+                message: "Student not found!"
+            }
+        }
+        if(!student.roomAlloted){
+            return {
+                error: true,
+                message: "Student is not allotted a room!"
+            }
+        }
+        const roomIssue = await RoomIssue.findOne({
+            studentid: studentid
+        }).select("-_id -__v").lean();
+        if(!roomIssue){
+            return {
+                error: true,
+                message: "No room issue found!"
+            }
+        }
+        return {
+            error: false,
+            data: roomIssue
+        };
+    }
+    catch(err){
+        Sentry.captureException(err);
+        await Sentry.flush(2000);
+        return {
+            error: true,
+            message: err
+        }
+    }
+}
