ed0bc24230ca656ba64d001db2272346867a1e7b
RaghhavDTurki <raghhavdturki@gmail.com>
2022-05-29 09:20:46 +0530

    Add Admin Dashboard Feature

---
+++ adminDashboard/index.ts
@@ -0,0 +1,69 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import { verifyToken } from "../src/utils/verifyToken";
+import { connect } from "../src/config/db.config";
+import { sentryInit } from "../src/config/sentry.config";
+import * as Sentry from "@sentry/node";
+import { adminDashboard } from "../src/controller/Admin/Dashboard";
+
+const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+    const HEADERS = { "Content-Type": "application/json" };
+    connect();
+    sentryInit();
+    try{
+        // Check for Token in Headers
+        const authToken = req.headers.authorization;
+        if(!authToken){
+            context.res = {
+                status: 401,
+                body: {
+                    message: "No authorization token provided!"
+                },
+                headers: HEADERS
+            };
+            return;
+        }
+        const unsealedToken = await verifyToken(authToken, "admin");
+        if(unsealedToken.error){
+            context.res = {
+                status: 401,
+                body: {
+                    message: "Unauthorized!"
+                },
+                headers: HEADERS
+            };
+            return;
+        }
+        const result = await adminDashboard();
+        if(result.error){
+            context.res = {
+                status: 400,
+                body: {
+                    message: result.message
+                },
+                headers: HEADERS
+            };
+            return;
+        }
+        else{
+            context.res = {
+                status: 200,
+                body: result.data,
+                headers: HEADERS
+            };
+            return;
+        }
+    }
+    catch(err){
+        Sentry.captureException(err);
+        context.res = {
+            status: 500,
+            body: {
+                message: err
+            },
+            headers: HEADERS
+        };
+    }
+
+};
+
+export default httpTrigger;
---
+++ src/controller/Admin/Dashboard.ts
@@ -0,0 +1,122 @@
+import * as Sentry from "@sentry/node";
+import { Room } from "../../models/Room.model";
+import { RoomIssue } from "../../models/RoomIssue.model";
+import { Hostel } from "../../models/Hostel.model";
+import { Warden } from "../../models/Warden.model";
+
+async function OccupancyRate(){
+    try{
+        const hostels = await Hostel.find({}).select("-_id -__v").lean();
+        const hostelids = hostels.map(hostel => hostel.hostelid);
+        let stats = [];
+        const cursor = await Room.aggregate([
+            {
+                $match: { "allotmentstatus": true }
+            },
+            {
+                $group: {
+                    _id: "$hostelid",
+                    count: { $sum: 1 }
+                }
+            }
+        ]);
+        for(const hostelid of hostelids){
+            let occupied = 0;
+            for(const stat of cursor){
+                if(stat._id == hostelid){
+                    occupied = stat.count;
+                    break;
+                }
+            }
+            const hostel = hostels.find(hostel => hostel.hostelid == hostelid);
+            const warden = await Warden.findOne({ hostelid: hostelid }).select("-_id -__v").lean();
+            const totalRooms = hostel.singleRooms + hostel.doubleRooms + hostel.tripleRooms;
+            const occupancyRate = (occupied / totalRooms) * 100;
+            stats.push({
+                hostelid: hostelid,
+                hostelname: hostel.name,
+                wardenid: hostel.wardenid,
+                wardenname: warden.name,
+                occupancyRate: occupancyRate
+            });
+        }
+        return {
+            error: false,
+            message: "Occupancy rate fetched successfully!",
+            data: stats
+        }
+    }
+    catch(err){
+        Sentry.captureException(err);
+        await Sentry.flush(2000);
+        return {
+            error: true,
+            message: err
+        }
+    }
+}
+
+async function IssueClearanceRate(){
+    try{
+        const hostels = await Hostel.find({}).select("-_id -__v").lean();
+        const hostelids = hostels.map(hostel => hostel.hostelid);
+        let stats = [];
+        for(const hostelid of hostelids)
+        {
+            const warden = await Warden.findOne({ hostelid: hostelid }).select("-_id -__v").lean();
+            const totalIssues = await RoomIssue.countDocuments({ hostelid: hostelid });
+            const solvedIssues = await RoomIssue.countDocuments({ hostelid: hostelid, status: "Resolved" });
+            const issueClearanceRate = (solvedIssues / totalIssues) * 100;
+            stats.push({
+                hostelid: hostelid,
+                hostelname: hostels.find(hostel => hostel.hostelid == hostelid).name,
+                wardenid: warden.wardenid,
+                wardenname: warden.name,
+                totalIssues: totalIssues,
+                issueClearanceRate: issueClearanceRate
+            });
+        }
+        return {
+            error: false,
+            message: "Issue clearance rate fetched successfully!",
+            data: stats
+        }
+    }
+    catch(err){
+        Sentry.captureException(err);
+        await Sentry.flush(2000);
+        return {
+            error: true,
+            message: err
+        }
+    }
+}
+
+export const adminDashboard = async () => {
+    try{
+        const occupancyRate = await OccupancyRate();
+        if(occupancyRate.error){
+            return occupancyRate;
+        }
+        const issueClearanceRate = await IssueClearanceRate();
+        if(issueClearanceRate.error){
+            return issueClearanceRate;
+        }
+        return {
+            error: false,
+            message: "Dashboard fetched successfully!",
+            data: {
+                occupancyRate: occupancyRate.data,
+                issueClearanceRate: issueClearanceRate.data
+            }
+        };
+    }
+    catch(err){
+        Sentry.captureException(err);
+        await Sentry.flush(2000);
+        return {
+            error: true,
+            message: err
+        };
+    }
+}
