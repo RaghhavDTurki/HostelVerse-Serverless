973c4251101522387b0aab0c8cbf1560fb745723
RaghhavDTurki <raghhavdturki@gmail.com>
2022-06-04 10:34:12 +0530

    Send Feedbacks along with hostel list

--- src/controller/Hostel/getHostelList.ts
+++ src/controller/Hostel/getHostelList.ts
@@ -1,21 +1,36 @@
 import { Hostel } from "../../models/Hostel.model";
 import * as Sentry from "@sentry/node";
+import { Feedback } from "../../models/Feedback.model";
 
 export async function getHostel(hostelid: string, low: number, high: number) {
     try{
         if(!hostelid){
             const hostels = await Hostel.find().select("-_id -__v").lean();
             const filteredList = hostels.filter(hostel => hostel.fees >= low && hostel.fees <= high);
+            const feedbacks = await Feedback.find().select("-_id -__v").lean();
+            let competeHostelList = [];
+            for(let i = 0; i < filteredList.length; i++){
+                const hostel = filteredList[i];
+                const feedback = feedbacks.filter(feedback => feedback.hostelid === hostel.hostelid);
+                competeHostelList.push({
+                    ...hostel,
+                    feedback: feedback
+                });
+            }
             return {
                 error: false,
-                data: filteredList
+                data: competeHostelList
             }
         }
         const hostel = await Hostel.findOne({ hostelid: hostelid }).select("-_id -__v").lean();
+        const feedback = await Feedback.find({ hostelid: hostelid }).select("-_id -__v").lean();
         return {
             error: false,
             message: "Hostel fetched successfully!",
-            data: hostel
+            data: {
+                ...hostel,
+                feedback: feedback
+            }
         }
     }
     catch(err){
