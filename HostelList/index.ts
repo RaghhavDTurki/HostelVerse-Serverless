52ca04670c521bb439f266e76a5684bf70147fbe
RaghhavDTurki <raghhavdturki@gmail.com>
2022-07-01 09:22:33 +0530

    Linting

--- HostelList/index.ts
+++ HostelList/index.ts
@@ -6,71 +6,71 @@ import * as Sentry from "@sentry/node";
 import { verifyToken } from "../src/utils/verifyToken";
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    let result: any;
-    const HEADERS = { "Content-Type": "application/json" };
-    connect();
-    sentryInit();
-    try {
-        // Check for Token in Headers
-        const authToken = req.headers.authorization;
-        if (!authToken) {
-            context.res = {
-                status: 401,
-                body: {
-                    message: "No authorization token provided!"
-                },
-                headers: HEADERS
-            };
-            return;
-        }
-        const unsealedToken = await verifyToken(authToken, "student", "admin");
-        if (unsealedToken.error) {
-            context.res = {
-                status: 401,
-                body: {
-                    message: "Unauthorized!"
-                },
-                headers: HEADERS
-            };
-            return;
-        }
-        const query = req.query["hostelid"];
-        const low = parseInt(req.query["low"]);
-        const high = parseInt(req.query["high"]);
-        result = await getHostel(query, low, high);
-        if (result.error) {
-            context.res = {
-                status: 400,
-                body: {
-                    message: result.message
-                },
-                headers: HEADERS
-            };
-        }
-        else {
-            context.res = {
-                status: 200,
-                body: {
-                    message: "Hostel fetched successfully!",
-                    data: result.data
-                },
-                headers: HEADERS
-            };
-        }
-    }
-    catch (err) {
-        Sentry.captureException(err);
-        await Sentry.flush(2000);
-        context.res = {
-            status: 500,
-            body: {
-                message: JSON.stringify({
-                    error: err.message
-                })
-            },
-            headers: HEADERS
-        };
-    }
+	let result: any;
+	const HEADERS = { "Content-Type": "application/json" };
+	connect();
+	sentryInit();
+	try {
+		// Check for Token in Headers
+		const authToken = req.headers.authorization;
+		if (!authToken) {
+			context.res = {
+				status: 401,
+				body: {
+					message: "No authorization token provided!"
+				},
+				headers: HEADERS
+			};
+			return;
+		}
+		const unsealedToken = await verifyToken(authToken, "student", "admin");
+		if (unsealedToken.error) {
+			context.res = {
+				status: 401,
+				body: {
+					message: "Unauthorized!"
+				},
+				headers: HEADERS
+			};
+			return;
+		}
+		const query = req.query["hostelid"];
+		const low = parseInt(req.query["low"]);
+		const high = parseInt(req.query["high"]);
+		result = await getHostel(query, low, high);
+		if (result.error) {
+			context.res = {
+				status: 400,
+				body: {
+					message: result.message
+				},
+				headers: HEADERS
+			};
+		}
+		else {
+			context.res = {
+				status: 200,
+				body: {
+					message: "Hostel fetched successfully!",
+					data: result.data
+				},
+				headers: HEADERS
+			};
+		}
+	}
+	catch (err) {
+		Sentry.captureException(err);
+		await Sentry.flush(2000);
+		context.res = {
+			status: 500,
+			body: {
+				message: JSON.stringify({
+					error: err.message
+				})
+			},
+			headers: HEADERS
+		};
+	}
 };
 
 export default httpTrigger;
