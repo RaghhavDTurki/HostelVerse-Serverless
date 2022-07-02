eded897353a35479bb004b00d78b9308440fd5f1
RaghhavDTurki <raghhavdturki@gmail.com>
2022-07-02 13:07:11 +0530

    Add feature to get payment history for a student

---
+++ getPaymentHistory/index.ts
@@ -0,0 +1,74 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions";
+import { connect } from "../src/config/db.config";
+import { sentryInit } from "../src/config/sentry.config";
+import * as Sentry from "@sentry/node";
+import { verifyToken } from "../src/utils/verifyToken";
+import { getPaymentHistory } from "../src/controller/Payment/getPaymentHistory";
+
+const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+    const HEADERS = { "Content-Type": "application/json" };
+    sentryInit();
+    connect();
+    try {
+        // Check for Token in Headers
+        const authToken = req.headers.authorization;
+        if (!authToken) {
+            context.res = {
+                status: 401,
+                body: {
+                    message: "No authorization token provided!"
+                },
+                headers: HEADERS
+            };
+            return;
+        }
+        const unsealedToken = await verifyToken(authToken, "admin", "student");
+        if (unsealedToken.error) {
+            context.res = {
+                status: 401,
+                body: {
+                    message: "Unauthorized!"
+                },
+                headers: HEADERS
+            };
+            return;
+        }
+
+        const studentid = req.query.studentid;
+        const result = await getPaymentHistory(studentid);
+        if (result.error) {
+            context.res = {
+                status: 400,
+                body: {
+                    message: result.message
+                },
+                headers: HEADERS
+            };
+            return;
+        }
+        else {
+            context.res = {
+                status: 200,
+                body: {
+                    message: result.data
+                },
+                headers: HEADERS
+            };
+            return;
+        }
+    }
+    catch (err) {
+        Sentry.captureException(err);
+        await Sentry.flush(2000);
+        context.res = {
+            status: 500,
+            body: {
+                error: true,
+                message: err.message
+            },
+            headers: HEADERS
+        };
+    }
+};
+
+export default httpTrigger;
---
+++ src/controller/Payment/getPaymentHistory.ts
@@ -0,0 +1,28 @@
+import * as Sentry from "@sentry/node";
+import { razorpayInstance } from "../../config/razorpay.config";
+import { Payment } from "../../models/Payment.model";
+
+export const getPaymentHistory = async (studentid: string) => {
+    try {
+        if (!studentid) {
+            return {
+                error: true,
+                message: "Student id is required!"
+            };
+        }
+        const payments = await Payment.find({ studentid: studentid, status: "Paid" }).select("-_id -__v").lean();
+        return {
+            error: false,
+            message: "Transaction history fetched successfully!",
+            data: payments
+        };
+    }
+    catch (err) {
+        Sentry.captureException(err);
+        await Sentry.flush(2000);
+        return {
+            error: true,
+            message: err.message
+        };
+    }
+};
