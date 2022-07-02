c31cf27ff0d512f0b5520ec80671f38691b19db8
RaghhavDTurki <raghhavdturki@gmail.com>
2022-07-02 13:07:30 +0530

    Add additional checks in createOrder

--- src/controller/Payment/createOrder.ts
+++ src/controller/Payment/createOrder.ts
@@ -3,6 +3,7 @@ import * as Sentry from "@sentry/node";
 import { CreateOrder } from "../../types/ValidationInput";
 import { genUUID } from "../../utils/createOTP";
 import { Payment } from "../../models/Payment.model";
+import { Student } from "../../models/Student.model";
 
 export const createOrder = async (body: CreateOrder) => {
     try {
@@ -12,7 +13,64 @@ export const createOrder = async (body: CreateOrder) => {
                 message: "No body provided"
             };
         }
-        const data = await razorpayInstance.orders.create({
+
+        // Checks required
+        // 1. If student with that studentid exists
+        // 2. If the student has paid the amount already for this semester(for production) -> Doing this to decrease the number of entries in the newOrderbase created by the same student -> If still some created entries exist at the end of the month, then delete them using a CRON job
+        // 3. If the student has created an order during this month -> If he/she has then fetch that 
+        // entry from newOrderbse and pass on the orderid and receiptid in response
+        // 4. Else create a new order and pass on the orderid and receiptid in response
+
+        // Check 1
+        const student = await Student.findOne({ studentid: body.studentid }).lean();
+        if (!student) {
+            return {
+                error: true,
+                message: "Student does not exist!"
+            };
+        }
+
+        // Check 2
+        const allOrders = await Payment.find({ studentid: body.studentid }).select("-_id -__v");
+        // const pastPayments = allOrders.filter(order => order.status === "Paid");
+        // if (pastPayments.length > 0) {
+        //     const lastUpdated = new Date(pastPayments[pastPayments.length - 1].updatedAt);
+        //     const currentDate = new Date();
+        //     const month = currentDate.getMonth();
+        //     const year = currentDate.getFullYear();
+        //     const lastUpdatedMonth = lastUpdated.getMonth();
+        //     const lastUpdatedYear = lastUpdated.getFullYear();
+        //     if (month === lastUpdatedMonth && year === lastUpdatedYear) {
+        //         return {
+        //             error: true,
+        //             message: "You have already paid for this semester!"
+        //         };
+        //     }
+        // }
+
+        // Check 3
+        const ordersCreated = allOrders.filter(order => order.status === "created");
+        if (ordersCreated.length > 0) {
+            const lastUpdated = new Date(ordersCreated[ordersCreated.length - 1].updatedAt);
+            const currentDate = new Date();
+            const month = currentDate.getMonth();
+            const year = currentDate.getFullYear();
+            const lastUpdatedMonth = lastUpdated.getMonth();
+            const lastUpdatedYear = lastUpdated.getFullYear();
+            if (month === lastUpdatedMonth && year === lastUpdatedYear) {
+                const order = ordersCreated[ordersCreated.length - 1];
+                return {
+                    error: false,
+                    message: "Order created successfully!",
+                    data: {
+                        orderid: order.orderid,
+                        receiptid: order.receiptid
+                    }
+                };
+            }
+        }
+
+        const newOrder = await razorpayInstance.orders.create({
             amount: parseInt(body.amount),
             currency: "INR",
             receipt: "rcpt" + genUUID(),
@@ -23,21 +81,21 @@ export const createOrder = async (body: CreateOrder) => {
         });
 
         const payment = new Payment({
-            orderid: data.id,
-            receiptid: data.receipt,
-            amount: data.amount,
+            orderid: newOrder.id,
+            receiptid: newOrder.receipt,
+            amount: newOrder.amount,
             studentid: body.studentid,
             hostelid: body.hostelid,
             email: body.email,
-            status: data.status
+            status: newOrder.status
         });
         await payment.save();
 
         return {
             error: false,
             data: {
-                orderid: data.id,
-                receiptid: data.receipt
+                orderid: newOrder.id,
+                receiptid: newOrder.receipt
             }
         };
     }
